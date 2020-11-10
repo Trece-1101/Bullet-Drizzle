966e13e9df57b79a5a5d4b27d8284277ff25d851
Trece-1101 <omar.bazzi.sf@gmail.com>
2020-11-09 22:12:22 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,6 +6,6 @@ self.onmessage = function (myData) {
             'Content-Type': 'application/json'
           },
         //body: JSON.stringify(obj)
-        body: JSON.stringify({game: "Bullet Drizzle", event: myData[0], data: myData[1]}) 
+        body: JSON.stringify({game: "Bullet Drizzle", event: myData.data[0], data: myData.data[1]}) 
     });
 }
